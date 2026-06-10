bf9ad5bd59102e6550c11fb523087ac7da0b1e02
Daniel Chockler <daniel.chockler@gmail.com>
2026-06-10 17:41:12 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+
+void echo(const std::string&);
+
+std::string getInput() {
+  std::string command {};
+  std::getline(std::cin, command);
+  return command;
+}
+
+std::string getCommand(const std::string& line) {
+  auto it {std::find_if(line.begin(), line.end(), [](char a) { return (a == ' '); } )};
+
+  return std::string {line.begin(), it};
+}
+
+bool processCommand(const std::string& command, const std::string& line) {
+  if (command == "echo") {
+    echo(line);
+    return true;
+  }
+  
+  return false;
+}
+
+void echo(const std::string& s) {
+  if (s.length() > static_cast<std::size_t>(5)) std::cout << s.substr(5) << '\n';
+  else std::cout << '\n';
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -8,11 +37,10 @@ int main() {
 
   while (true) {
     std::cout << "$ ";
-    std::string command {};
-    std::cin >> command;
+    std::string input {getInput()};
+    std::string command {getCommand(input)};
   
     if (command == "exit") break;
-
-    std::cout << command << ": command not found\n";
+    if (!processCommand(command, input)) std::cout << command << ": command not found\n";
   }
 }
